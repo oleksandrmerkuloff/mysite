e63f979cef89a1f617dbc26a373f747403e3a1fa
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-06-22 16:41:56 +0300

    create models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField()
+    text = models.TextField()
+    category = models.ManyToOneRel(Category)
+
