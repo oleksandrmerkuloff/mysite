e344c58e5768c53fd6b088a45d6ac2f7bf3bd8af
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-06-24 19:45:30 +0300

    clear empty data

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-
-
-class Category(models.Model):
-    title = models.CharField(max_length=50)
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=150)
-    image = models.ImageField()
-    text = models.TextField()
-    category = models.ManyToOneRel(Category)
