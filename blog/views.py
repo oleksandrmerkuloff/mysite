a1c60db73b21687919084d85c5b2d99c5d3a6b90
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-06-26 04:39:03 +0300

    add views for single page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 
 def index(request):
     return render(request, template_name="index.html")
@@ -10,9 +12,17 @@ def blog(request):
     return HttpResponse("Blog page")
 
 
+def single_post(request, post_id):
+    post_to_open = models.Post.objects.get(id=post_id)
+    return render()
+
+
 def portfolio(request):
     return HttpResponse("Portfolio page")
 
+def single_project(request, project_id):
+    project_to_open = models.Project.objects.get(id=id)
+    return render()
 
 def contacts(request):
     return HttpResponse("Contact page")
