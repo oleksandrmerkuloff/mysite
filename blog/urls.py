b9df725516f1cccb32e8ec6dd4961a95c1e51638
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-06-26 04:41:15 +0300

    fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('blog/', views.blog),
+    path('blog/<post_id:int>', views.single_post),
     path('portfolio/', views.portfolio),
+    path('portfolio/<project_id:int>', views.single_project),
     path('contacts/', views.contacts),
 ]
